e83aafc0273912be13f10e2c9545e5f070255b93
andony89 <74324817+andony89@users.noreply.github.com>
2025-04-04 02:08:56 -0600

    test
    andony

---
+++ WebBiblioteca/Models/RoleModels.cs
@@ -0,0 +1,19 @@
+﻿namespace WebBiblioteca.Models
+{
+    public class RoleModels
+    {
+        public const int ADMIN = 1;
+        public const int CLIENTE = 2;
+        public const int EMPLEADO = 3;
+
+        public static bool IsAdmin(HttpContext context) =>
+            context.Session.GetInt32("UserRole") == ADMIN;
+
+        public static bool IsCliente(HttpContext context) =>
+            context.Session.GetInt32("UserRole") == CLIENTE;
+
+        public static bool isEmpleado(HttpContext context) =>
+            context.Session.GetInt32("UserRole") == EMPLEADO;
+
+    }
+}
